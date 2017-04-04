7ae67b051208c1dbaa77337a9dd2d6676e750e14
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2017-04-04 11:42:16 -0400

    almost finished

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,2 +1,4 @@
+$( document ).ready(function() {
+	    // $(".fancy_title").circleType();
 
-
+});
--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/contact', function (req,res) {
 	res.sendFile('html/contact.html', {"root":__dirname});
 });
 
+app.get('/aboutme', function (req,res) {
+	res.sendFile('html/about.html', {"root":__dirname});
+});
+
+
 app.listen(3000, function(){
 	console.log("Express is Listening on Portal 3000");
 })
