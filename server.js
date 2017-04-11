e6a5f59e5ec50ca09ea8e5f0b3ae36ded84cc967
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2017-04-10 22:00:33 -0400

    again

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
+// Dependencies
+// ===========================================================
 var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 var nodemailer = require('nodemailer');
-
-
+// Sets up the Express App
+// =============================================================
 var app = express();
+var PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// ===========================================================
 app.use(express.static('public'));
 app.use(express.static('javascript'));
 
-//accesing the home page root
+//accessing the html pages
+// ===========================================================
+
 app.get('/', function(req,res){
 	res.sendFile('html/index.html',{"root":__dirname});
 });
@@ -19,8 +32,20 @@ app.get('/contact', function (req,res) {
 app.get('/aboutme', function (req,res) {
 	res.sendFile('html/about.html', {"root":__dirname});
 });
+//nodemailer 
+// ===========================================================
 
 
-app.listen( process.env. PORT || 3000, function(){
+
+
+
+
+
+
+
+//Server is listening.
+// ===========================================================
+
+app.listen(PORT, function(){
 	console.log("Express is Listening on Portal 3000");
 })
