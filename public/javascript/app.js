ba8b346a4bd5c2d686a9a2430db1764e73b1fe35
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2017-04-18 18:36:51 -0400

    nodemailer

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,4 +1,32 @@
 $( document ).ready(function() {
-	    // $(".fancy_title").circleType();
 
+	// Contact.html to send email using nodemailer
+	$('#send_email').click(function(){
+		
+		var name = $('#name').val();
+		var lastname = $('#lastname').val();
+		var phone = $('#phone').val();
+		var email = $('#email').val();
+		var message = $('#message').val();
+		console.log(name, lastname, phone, email, message);
+		if (name !==  '' && lastname !== '' && phone !== '' && email !== '' && message !== ''){
+			console.log('post');
+			$.post('http://localhost:3000/contact', 
+			{
+				name: name,
+				lastname: lastname,
+				phone: phone,
+				email: email,
+				message: message
+			}
+			, function(data) {
+				if(data == 'sent') {
+					$('.alert').html('<strong> Success! </strong> Your message has been sent!').addClass('sucess');
+				} else {
+					$('.alert').html("<strong> Ups! </strong> Your message didn't go through ").addClass('error');
+				}
+			});
+
+		}
+	});
 });
--- server.js
+++ server.js
@@ -4,25 +4,33 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var http = require('http');
+
+
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+	extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({
+	type:'application/vnd.api+json'
+}));
 
 // ===========================================================
 app.use(express.static('public'));
 app.use(express.static('javascript'));
 
-//accessing the html pages
+//accessing the Front/html pages
 // ===========================================================
 
 app.get('/', function(req,res){
 	res.sendFile('html/index.html',{"root":__dirname});
+	console.log("nodemailer reading" + req.url);
 });
 
 app.get('/contact', function (req,res) {
@@ -32,17 +40,48 @@ app.get('/contact', function (req,res) {
 app.get('/aboutme', function (req,res) {
 	res.sendFile('html/about.html', {"root":__dirname});
 });
-//nodemailer 
+//nodemailer sending mail functions
 // ===========================================================
+//sending mail function
+	// sending mail function
+app.post('/contact', function(req, res){
+if(req.body.email == "" || req.body.subject == "") {
+  res.send("Error: Email & Subject should not blank");
+  return false;
+}
+if (req.body.email) {
+	res.send(req.body.email);
+}
+res.send(req.body);
 
+   // Sending Emails with SMTP, Configuring SMTP settings
 
-
-
-
-
-
-
-
+    var smtpTransport = nodemailer.createTransport({
+             host: "smtp.gmail.com", // hostname
+    secureConnection: true, // use SSL
+    port: 465, // port for secure SMTP
+            auth: {
+                 user: 'sanquinluis@gmail.com',
+                 pass: ''
+            }
+        });
+        var mailOptions = {
+            from: "Node Emailer ✔ <sanquinluis@gmail.com>", // sender address
+            to: req.body.email, // list of receivers
+            subject: req.body.subject+" ✔", // Subject line
+            //text: "Hello world ✔", // plaintext body
+            html: "<b>"+req.body.description+"</b>" // html body
+        }
+        smtpTransport.sendMail(mailOptions, function(error, response){
+        if(error){
+             res.send("Email could not sent due to error: "+error);
+        }else{
+             res.send("Email has been sent successfully");
+        } 
+    }); 
+});
+		
+// sending email using SMTP configuration
 //Server is listening.
 // ===========================================================
 
